83016dc2356890e88af4b093550427c77313692f
TheMinmusDerp <71770907+TheMinmusDerp@users.noreply.github.com>
2021-12-10 08:40:06 -0600

    Add files via upload

--- swappingFiles.py
+++ swappingFiles.py
@@ -1,6 +1,6 @@
 def swapFileData():
-  file1='/Desktop/C98/sample1.txt'
-  file2='/Desktop/C98/sample2.txt'
+  file1=input("Input the filepath for file 1 ")
+  file2=input("Input the filepath for file 2 ")
   with open(file1,'r') as a:
     data_a = a.read()
   with open(file2,'r') as b:
@@ -9,3 +9,4 @@ def swapFileData():
     a.write(data_b)
   with open(file2,'w') as b:
     b.write(data_a)
+swapFileData()
